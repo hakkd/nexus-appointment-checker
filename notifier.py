19038029e6c538203abe32a356410dcb2523a342
Elmeri Hakkinen <60952417+hakkd@users.noreply.github.com>
2024-08-02 17:14:10 -0700

    added macos notification popup when results returned

--- notifier.py
+++ notifier.py
@@ -6,9 +6,21 @@ import os
 class Notifier:
 
     @staticmethod
-    def notify(message):
+    def __notify_macos(message):
+        title = "NEXUS Interview Alert"
+        command = f"""
+        osascript -e 'display notification "{message}" with title "{title}"'
+        """
+        os.system(command)
+
+    @staticmethod
+    def __notify_whatsapp(message):
         load_dotenv()
         contacts = os.getenv("CONTACTS").split(", ")
         for contact in contacts:
             print(contact)
             kit.sendwhatmsg_instantly(contact, message)
+
+    @staticmethod
+    def notify(message):
+        Notifier.__notify_macos(message)
